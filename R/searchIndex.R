7c91145eaa152800b5dc428a3ced5f29ee499752
jryan <jryan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-14 06:39:50 +0000

    o  added range query support to searchIndex by specifying    an x= value of length 2.  This defaults to [x(1),x(2)]    by creating a type=c(">=","<=")
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/indexing@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/searchIndex.R
+++ R/searchIndex.R
@@ -1,53 +1,70 @@
 searchIndex <-
-function(column, x, type='=', SIZE=1e5, env=.IndexEnv) {
-  # only applicable to equality currently
-  if(missing(column))
-    stop("column must be specified")
-  #.x <- get(".IndexEnv", .GlobalEnv)[[column]]
+function(column, x, type='=', SIZE=1e5, env=.IndexEnv, range=FALSE) {
+  if(missing(column) || !is.character(column))
+    stop("column must be specified as character string")
   .x <- env[[column]]
   binsearch <- xts:::binsearch
-  if(!is.null(.x$l) && is.character(.x$l)) {
-    x <- which(x==.x$l) 
-  } 
-  sparse_seq <- seq(1, length(.x$s), SIZE)
-  if(sparse_seq[length(sparse_seq)] != length(.x$s))
-    sparse_seq <- c(sparse_seq,length(.x$s))
 
-  sparse_index <- .x$s[sparse_seq]
-
-  ll <- binsearch(x, sparse_index,TRUE)
-  if(ll != 1)
-    ll <- ll-1L
-  ul <- binsearch(x, sparse_index,FALSE)
-  if(ul != length(sparse_index))
-    ul <- ul+1L
-
-  final_index <- seq(sparse_seq[ll],sparse_seq[ul])
-  s_final_index <- .x$s[final_index]
-
-  # avoid unnecessary binsearch for inequality
-  if(type %in% c("=","==","!=","<",">="))
-    x_start <- binsearch(x, s_final_index, TRUE)
-
-  if(type %in% c("=","==","!=",">","<="))
-    x_end   <- binsearch(x, s_final_index, FALSE)
-
-  if(type %in% c("=", "==", "!=")) {
-    i <- .Call("R_memcpy", final_index, x_start, x_end)
-    if(type == "!=")
-      i <- (1:length(.x$s))[-i]
-  } else
-  if(type == "<") {
-    i <- seq(1,final_index[x_start]-1)
-  } else
-  if(type == "<=") {
-    i <- seq(1,final_index[x_end])
-  } else
-  if(type == ">") {
-    i <- seq(final_index[x_end]+1, length(.x$s))
-  } else
-  if(type == ">=") {
-    i <- seq(final_index[x_start], length(.x$s))
+  if(length(x) == 2) { # range query
+    if(missing(type))
+      type <- c(">=","=<")
+    if(length(type) != 2L) stop("length of 'type' and 'x' must match")
+    i <- seq(searchIndex(column, x[1], type=type[1], SIZE, env, range=TRUE),
+             searchIndex(column, x[2], type=type[2], SIZE, env, range=TRUE))
+  } else { # non-range query
+    if(!is.null(.x$l) && is.character(.x$l)) {
+      x <- which(x==.x$l) 
+    } 
+    sparse_seq <- seq(1, length(.x$s), SIZE)
+    if(sparse_seq[length(sparse_seq)] != length(.x$s))
+      sparse_seq <- c(sparse_seq,length(.x$s))
+  
+    sparse_index <- .x$s[sparse_seq]
+  
+    ll <- binsearch(x, sparse_index,TRUE)
+    if(ll != 1)
+      ll <- ll-1L
+    ul <- binsearch(x, sparse_index,FALSE)
+    if(ul != length(sparse_index))
+      ul <- ul+1L
+  
+    final_index <- seq(sparse_seq[ll],sparse_seq[ul])
+    s_final_index <- .x$s[final_index]
+  
+    # avoid unnecessary binsearch for inequality
+    if(type %in% c("=","==","!=","<",">="))
+      x_start <- binsearch(x, s_final_index, TRUE)
+  
+    if(type %in% c("=","==","!=",">","<="))
+      x_end   <- binsearch(x, s_final_index, FALSE)
+  
+    if(type %in% c("=", "==", "!=")) {
+      i <- .Call("R_memcpy", final_index, x_start, x_end)
+      if(type == "!=")
+        i <- (1:length(.x$s))[-i]
+    } else
+    if(type == "<") {
+      if(range)
+        return(final_index[x_start]-1)
+      i <- seq(1,final_index[x_start]-1)
+    } else
+    if(type == "<=") {
+      if(range)
+        return(final_index[x_end])
+      i <- seq(1,final_index[x_end])
+    } else
+    if(type == ">") {
+      if(range)
+        return(final_index[x_end])
+      i <- seq(final_index[x_end]+1, length(.x$s))
+    } else
+    if(type == ">=") {
+      if(range)
+        return(final_index[x_start])
+      i <- seq(final_index[x_start], length(.x$s))
+    } else {
+      stop(paste('type:',type,'not one of [==, !=, =, <, <=, >, >=]'))
+    }
   }
 
   if(length(i)==2 && i[2] > length(.x$s))
