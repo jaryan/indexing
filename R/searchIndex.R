0c0e478d5da4e44bdb05187379595758dc91c6cf
jryan <jryan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-14 06:05:55 +0000

    o  added 'type' to searchIndex that allows for    ==, =, !=, <, <=, >, >= queries on indexed data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/indexing@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/searchIndex.R
+++ R/searchIndex.R
@@ -1,5 +1,5 @@
 searchIndex <-
-function(column, x, SIZE=1e5, env=.IndexEnv) {
+function(column, x, type='=', SIZE=1e5, env=.IndexEnv) {
   # only applicable to equality currently
   if(missing(column))
     stop("column must be specified")
@@ -24,11 +24,33 @@ function(column, x, SIZE=1e5, env=.IndexEnv) {
 
   final_index <- seq(sparse_seq[ll],sparse_seq[ul])
   s_final_index <- .x$s[final_index]
-  #i <- seq(binsearch(x, s_final_index, FALSE), binsearch(x, s_final_index, TRUE))
-  #i <- final_index[i]
-  i <- .Call("R_memcpy", final_index, binsearch(x,s_final_index,TRUE), binsearch(x,s_final_index,FALSE))
+
+  # avoid unnecessary binsearch for inequality
+  if(type %in% c("=","==","!=","<",">="))
+    x_start <- binsearch(x, s_final_index, TRUE)
+
+  if(type %in% c("=","==","!=",">","<="))
+    x_end   <- binsearch(x, s_final_index, FALSE)
+
+  if(type %in% c("=", "==", "!=")) {
+    i <- .Call("R_memcpy", final_index, x_start, x_end)
+    if(type == "!=")
+      i <- (1:length(.x$s))[-i]
+  } else
+  if(type == "<") {
+    i <- seq(1,final_index[x_start]-1)
+  } else
+  if(type == "<=") {
+    i <- seq(1,final_index[x_end])
+  } else
+  if(type == ">") {
+    i <- seq(final_index[x_end]+1, length(.x$s))
+  } else
+  if(type == ">=") {
+    i <- seq(final_index[x_start], length(.x$s))
+  }
+
   if(length(i)==2 && i[2] > length(.x$s))
     return(logical(0))
   .x$o[i]
 }
-
